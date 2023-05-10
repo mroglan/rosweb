f885e8241b6309f3e312dba7153196f0210a7288
Manuel Roglan <manueljoseph113@gmail.com>
2023-05-10 17:43:54 -0400

    added typescript

--- client/src/App.js
+++
@@ -1,8 +0,0 @@
-
-export default function App() {
-  return (
-    <div>
-      hello world
-    </div>
-  );
-}
---
+++ client/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Home from './pages'
+
+export default function App() {
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Home />} />
+			</Routes>	
+		</BrowserRouter>
+	)
+}
