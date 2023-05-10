a752690c66e9e49255c7ce7e8bb0591d388eacf9
Manuel Roglan <manueljoseph113@gmail.com>
2023-05-10 18:19:37 -0400

    using mui

--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,7 +1,12 @@
 import React from 'react'
+import {Box, Paper, Typography} from '@mui/material'
 
 export default function Home() {
-    return <div>
-        home page
-    </div>
+    return (
+        <Box>
+            <Box textAlign="center">
+                <Typography variant="h2">RosWeb</Typography>
+            </Box>
+        </Box>
+    )
 }
---
+++ client/src/styles/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    palette: {
+        primary: {
+            main: 'hsl(246,79%,44%)' 
+        },
+        background: {
+            default: 'hsl(246,100%,98%)',
+            paper: '#fff'
+        }
+    }
+})
