04df4a14c982ea4bf64653b672741ba28d00a3d5
Manuel Roglan <manueljoseph113@gmail.com>
2023-05-10 20:32:46 -0400

    displaying availabe options

---
+++ client/src/components/index/Options.tsx
@@ -0,0 +1,29 @@
+import { Box, Paper, Typography } from "@mui/material";
+import { Link } from "react-router-dom";
+
+const options = [
+    {name: 'Bagged Image to Video', link: '/bagged-image-to-video'}
+].sort((a,b) => a.name.localeCompare(b.name))
+
+export default function Options() {
+
+    return (
+        <Box>
+            <Box maxWidth="sm" mx="auto">
+                {options.map((option, i) => (
+                    <Box key={i} my={1}>
+                        <Paper elevation={3}>
+                            <Link to={option.link}>
+                                <Box p={1} textAlign="center">
+                                    <Typography variant="h6">
+                                        {option.name}
+                                    </Typography>
+                                </Box>
+                            </Link>
+                        </Paper>
+                    </Box>
+                ))}
+            </Box>
+        </Box>
+    )
+}
--- client/src/pages/index.tsx
+++ client/src/pages/index.tsx
@@ -1,12 +1,23 @@
 import React from 'react'
-import {Box, Paper, Typography} from '@mui/material'
+import {Box, Container, Paper, Typography} from '@mui/material'
+import Options from '../components/index/Options'
 
 export default function Home() {
     return (
         <Box>
-            <Box textAlign="center">
-                <Typography variant="h2">RosWeb</Typography>
-            </Box>
+            <Container maxWidth="md">
+                <Box textAlign="center">
+                    <Typography variant="h2">RosWeb</Typography>
+                </Box>
+                <Box mt={3} textAlign="center">
+                    <Typography variant="h4">
+                        Available Tools
+                    </Typography>
+                </Box>
+                <Box mt={3}>
+                    <Options />
+                </Box>
+            </Container>
         </Box>
     )
 }
