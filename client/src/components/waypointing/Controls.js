5ad02c8877f17ca5106d73574cc96e55029978f7
Manuel Roglan <manueljoseph113@gmail.com>
2023-08-03 21:03:33 -0400

    sending topics to server

--- client/src/components/waypointing/Controls.js
+++ client/src/components/waypointing/Controls.js
@@ -5,6 +5,7 @@ import EditIcon from '@mui/icons-material/Edit';
 import ExitToAppIcon from '@mui/icons-material/ExitToApp';
 import { Box } from "@mui/material";
 import { BlueIconButtonGroup } from "../misc/buttons";
+import Settings from "./Settings";
 
 const tabOptions = [
     {value: 'settings', icon: <SettingsIcon fontSize="large" />},
@@ -13,14 +14,17 @@ const tabOptions = [
     {value: 'export', icon: <ExitToAppIcon fontSize="large" />}
 ]
 
-export default function Controls({ws}) {
+export default function Controls({ws, controls, setControls}) {
 
-    const [tab, setTab] = useState("settings")
+    const [tab, setTab] = useState('settings')
 
     return (
         <Box>
             <Box>
                 <BlueIconButtonGroup options={tabOptions} selected={tab} setSelected={setTab} />
+            </Box>
+            <Box>
+                {tab == 'settings' ? <Settings ws={ws} controls={controls} setControls={setControls} /> : ''}
             </Box>
         </Box>
     )
--- client/src/components/waypointing/Main.js
+++ client/src/components/waypointing/Main.js
@@ -1,13 +1,19 @@
 import { Box, Grid } from "@mui/material";
 import Controls from "./Controls";
+import { useState } from "react";
 
 export default function Main({ws}) {
+
+    const [controls, setControls] = useState({
+        gpsTopic: '',
+        odomTopic: ''
+    })
 
     return (
         <Box>
             <Grid container spacing={1} wrap="nowrap" minWidth={1300}>
                 <Grid item width={500}>
-                    <Controls ws={ws} />
+                    <Controls ws={ws} controls={controls} setControls={setControls} />
                 </Grid>
                 <Grid item minWidth={800} flex={1}>
                     <Box bgcolor="red">
